08ea34ac143024dcffcb415bf7c0af6032824ca7
kopachmaksym <93545242+kopachmaksym@users.noreply.github.com>
2023-04-26 21:37:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ dict['a-chip PREMIUM cannula'] = 0
 dict['a-chip PREMIUM mini'] = 0
 dict['animal-id pro 2,12x12mm'] = 0
 
-prices['a-chip PREMIUM'] = 16500
-prices['Datamars'] = 18200
-prices['animal-id min'] = 11000
-prices['a-chip PREMIUM cannula'] = 13400
-prices['a-chip PREMIUM mini'] = 18200
-prices['animal-id pro 2,12x12mm'] = 10200
+prices['a-chip PREMIUM'] = 165
+prices['Datamars'] = 182
+prices['animal-id min'] = 110
+prices['a-chip PREMIUM cannula'] = 134
+prices['a-chip PREMIUM mini'] = 182
+prices['animal-id pro 2,12x12mm'] = 102
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -45,12 +45,20 @@ let counter4 = document.getElementById("counter4");
 let counter5 = document.getElementById("counter5");
 let counter6 = document.getElementById("counter6");
 
+let order1 = document.getElementById("order1");
+let order2 = document.getElementById("order2");
+let order3 = document.getElementById("order3");
+let order4 = document.getElementById("order4");
+let order5 = document.getElementById("order5");
+let order6 = document.getElementById("order6");
+
 btn1.addEventListener("click", function (){
 
     btn1.style.display = "none";
     plus_minus1.style.display = "flex";
     counter1.style.display = "flex";
     counter1.innerText = "1";
+    order1.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
@@ -59,20 +67,6 @@ btn1.addEventListener("click", function (){
     }
     dict['a-chip PREMIUM'] = 1;
 
-    // let v = "";
-    // for (const [key, value] of Object.entries(dict)){
-    //     v = 1;
-    // }
-    // alert(typeof toString(v));
-
-    // if (tg.MainButton.isVisible){
-    //     tg.MainButton.hide();
-    // }
-    // else {
-    //     tg.MainButton.setText('Ви вибрали товар 1!');
-    //     item = "1";
-    //     tg.MainButton.show();
-    // }
 })
 
 btn2.addEventListener("click", function (){
@@ -81,6 +75,7 @@ btn2.addEventListener("click", function (){
     plus_minus2.style.display = "flex";
     counter2.style.display = "flex";
     counter2.innerText = "1";
+    order2.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
@@ -96,6 +91,7 @@ btn3.addEventListener("click", function (){
     plus_minus3.style.display = "flex";
     counter3.style.display = "flex";
     counter3.innerText = "1";
+    order3.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
@@ -103,14 +99,7 @@ btn3.addEventListener("click", function (){
         tg.MainButton.show();
     }
     dict['animal-id min'] = 1;
-    // if (tg.MainButton.isVisible){
-    //     tg.MainButton.hide();
-    // }
-    // else {
-    //     tg.MainButton.setText('Ви вибрали товар 3!');
-    //     item = "3";
-    //     tg.MainButton.show();
-    // }
+
 })
 
 btn4.addEventListener("click", function (){
@@ -119,20 +108,14 @@ btn4.addEventListener("click", function (){
     plus_minus4.style.display = "flex";
     counter4.style.display = "flex";
     counter4.innerText = "1";
+    order4.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
         tg.MainButton.show();
     }
     dict['a-chip PREMIUM cannula'] = 1;
-    // if (tg.MainButton.isVisible){
-    //     tg.MainButton.hide();
-    // }
-    // else {
-    //     tg.MainButton.setText('Ви вибрали товар 4!');
-    //     item = "4";
-    //     tg.MainButton.show();
-    // }
+
 })
 
 btn5.addEventListener("click", function (){
@@ -141,20 +124,13 @@ btn5.addEventListener("click", function (){
     plus_minus5.style.display = "flex";
     counter5.style.display = "flex";
     counter5.innerText = "1";
+    order5.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
         tg.MainButton.show();
     }
     dict['a-chip PREMIUM mini'] = 1;
-    // if (tg.MainButton.isVisible){
-    //     tg.MainButton.hide();
-    // }
-    // else {
-    //     tg.MainButton.setText('Ви вибрали товар 5!');
-    //     item = "5";
-    //     tg.MainButton.show();
-    // }
 })
 
 btn6.addEventListener("click", function (){
@@ -163,20 +139,14 @@ btn6.addEventListener("click", function (){
     plus_minus6.style.display = "flex";
     counter6.style.display = "flex";
     counter6.innerText = "1";
+    order6.style.display = "";
 
     if (!tg.MainButton.isVisible){
         tg.MainButton.setText('Купити');
         tg.MainButton.show();
     }
     dict['animal-id pro 2,12x12mm'] = 1;
-    // if (tg.MainButton.isVisible){
-    //     tg.MainButton.hide();
-    // }
-    // else {
-    //     tg.MainButton.setText('Ви вибрали товар 6!');
-    //     item = "6";
-    //     tg.MainButton.show();
-    // }
+
 })
 
 let plus1 = document.getElementById("plus1");
@@ -190,13 +160,15 @@ plus1.addEventListener("click", function () {
 
     counter1.innerText = Number(counter1.innerText) + 1;
     dict['a-chip PREMIUM'] = dict['a-chip PREMIUM'] +1 ;
+    order1.getElementsByClassName("order-count").innerText = Number(counter1.innerText) + "x";
 
 })
 
 plus2.addEventListener("click", function () {
 
     counter2.innerText = Number(counter2.innerText) + 1;
-    dict['Datamars'] = dict['Datamars'] +1 ;
+    dict['Datamars'] = dict['Datamars'] +1;
+    order2.getElementsByClassName("order-count").innerText = Number(counter2.innerText) + "x";
 
 })
 
@@ -204,6 +176,7 @@ plus3.addEventListener("click", function () {
 
     counter3.innerText = Number(counter3.innerText) + 1;
     dict['animal-id min'] = dict['animal-id min'] +1 ;
+    order3.getElementsByClassName("order-count").innerText = Number(counter3.innerText) + "x";
 
 })
 
@@ -211,6 +184,7 @@ plus4.addEventListener("click", function () {
 
     counter4.innerText = Number(counter4.innerText) + 1;
     dict['a-chip PREMIUM cannula'] = dict['a-chip PREMIUM cannula'] +1 ;
+    order4.getElementsByClassName("order-count").innerText = Number(counter4.innerText) + "x";
 
 })
 
@@ -218,6 +192,7 @@ plus5.addEventListener("click", function () {
 
     counter5.innerText = Number(counter5.innerText) + 1;
     dict['a-chip PREMIUM mini'] = dict['a-chip PREMIUM mini'] +1 ;
+    order5.getElementsByClassName("order-count").innerText = Number(counter5.innerText) + "x";
 
 })
 
@@ -225,6 +200,7 @@ plus6.addEventListener("click", function () {
 
     counter6.innerText = Number(counter6.innerText) + 1;
     dict['animal-id pro 2,12x12mm'] = dict['animal-id pro 2,12x12mm'] +1 ;
+    order6.getElementsByClassName("order-count").innerText = Number(counter6.innerText) + "x";
 
 })
 
@@ -243,6 +219,7 @@ minus1.addEventListener("click", function () {
         btn1.style.display = "inline-block";
         plus_minus1.style.display = "none";
         counter1.style.display = "none";
+        order1.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -262,6 +239,7 @@ minus2.addEventListener("click", function () {
         btn2.style.display = "inline-block";
         plus_minus2.style.display = "none";
         counter2.style.display = "none";
+        order2.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -281,6 +259,7 @@ minus3.addEventListener("click", function () {
         btn3.style.display = "inline-block";
         plus_minus3.style.display = "none";
         counter3.style.display = "none";
+        order3.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -300,6 +279,7 @@ minus4.addEventListener("click", function () {
         btn4.style.display = "inline-block";
         plus_minus4.style.display = "none";
         counter4.style.display = "none";
+        order4.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -319,6 +299,7 @@ minus5.addEventListener("click", function () {
         btn5.style.display = "inline-block";
         plus_minus5.style.display = "none";
         counter5.style.display = "none";
+        order5.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -338,6 +319,7 @@ minus6.addEventListener("click", function () {
         btn6.style.display = "inline-block";
         plus_minus6.style.display = "none";
         counter6.style.display = "none";
+        order6.style.display = "none";
     }
 
     let sum = Number(counter1.innerText) + Number(counter2.innerText) + Number(counter3.innerText);
@@ -350,11 +332,21 @@ minus6.addEventListener("click", function () {
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function (){
-    let v = "";
-    for (const [key, value] of Object.entries(dict)){
-        v = v + "#" + key + ":" + value + ":" + prices[key];
+
+
+    if(document.getElementsByClassName("inner").style.display === "grid"){
+        document.getElementsByClassName("inner").style.display = "none";
+        document.getElementsByClassName("basket").style.display = "";
     }
-    tg.sendData(v);
+    else {
+        let v = "";
+        for (const [key, value] of Object.entries(dict)){
+            v = v + "#" + key + ":" + value + ":" + prices[key];
+        }
+        tg.sendData(v);
+    }
+
+
 })
 let usercard = document.getElementById("usercard");
 
@@ -363,3 +355,13 @@ let p = document.createElement("p");
 p.innerText = tg.initDataUnsafe.first_name + tg.DataUnsafe.last_name;
 
 usercard.appendChild(p);
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
+
+function click_edit(){
+    document.getElementsByClassName("inner").style.display = "grid";
+    document.getElementsByClassName("basket").style.display = "none";
+}
