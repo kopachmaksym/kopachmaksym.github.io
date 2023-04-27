6414c34236f8ace49fd5f5bff268b188e9b16e9e
kopachmaksym <93545242+kopachmaksym@users.noreply.github.com>
2023-04-27 09:43:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -350,13 +350,13 @@ Telegram.WebApp.onEvent("mainButtonClicked", function (){
     }
 })
 
-let usercard = document.getElementById("usercard");
+// let usercard = document.getElementById("usercard");
 
-let p = document.createElement("p");
+// let p = document.createElement("p");
 
-p.innerText = tg.initDataUnsafe.first_name + tg.DataUnsafe.last_name;
+// p.innerText = tg.initDataUnsafe.first_name + tg.DataUnsafe.last_name;
 
-usercard.appendChild(p);
+// usercard.appendChild(p);
 
 function auto_grow(element) {
     element.style.height = "5px";
