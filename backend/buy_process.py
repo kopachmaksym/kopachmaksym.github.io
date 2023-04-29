32b0bf331861307f97d1ac3d2728c9eb73fc6cb3
kopachmaksym <mk410404@gmail.com>
2023-04-29 21:51:39 +0300

    Testing payment

--- backend/buy_process.py
+++ backend/buy_process.py
@@ -6,7 +6,11 @@ loop = asyncio.new_event_loop()
 bot = Bot(TOKEN, parse_mode='html')
 dp = Dispatcher(bot, loop)
 
+# admin_loop = asyncio.new_event_loop()
+# admin_bot = Bot(TOKEN, parse_mode='html')
+# admin_dp = Dispatcher(admin_bot, admin_loop)
 
 if __name__ == '__main__':
     from handlers import dp
     executor.start_polling(dp)
+    # executor.start_polling(admin_dp)
--- backend/handlers.py
+++ backend/handlers.py
@@ -1,15 +1,17 @@
-from buy_process import dp, bot
+from buy_process import dp, bot, admin_dp, admin_bot
 from config import PAYMENT_TOKEN
 from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, Invoice
 from aiogram.types.message import ContentType
 import re
 from keyboads import keyboard
 
+data_post = {}
+
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
     await bot.send_message(message.chat.id, 'Будь ласка, виберіть опцію:', reply_markup=keyboard)
 
-@dp.message_handler(content_types='web_app_data' )
+@dp.message_handler(content_types='web_app_data')
 async def buy_process(web_app_message):
     # print(web_app_message.web_app_data.data)
     m = re.search(r' <comment>.*<comment>', web_app_message.web_app_data.data)
@@ -18,11 +20,12 @@ async def buy_process(web_app_message):
     data = web_app_message.web_app_data.data.split('#')
     data = [i.split(':') for i in data]
     data.pop(0)
-
+    sum = 0
     for item in data:
         if item[1] == '0':
             continue
         PRICE.append(LabeledPrice(label=f'{item[0]} x{item[1]}', amount=int(item[2])*int(item[1])*100))
+        sum += item[2]
 
     await bot.send_invoice(web_app_message.chat.id,
                            title='Покупка мікрочіпів',
@@ -38,12 +41,27 @@ async def buy_process(web_app_message):
                                      'images?q=tbn:ANd9GcSxeOQ6x3CbY3R1lhNWI_s6dmi1Qcg7zocY_g&usqp=CAU',
                            payload='some_invoice')
 
+
+    data_post['prices'] = PRICE
+    data_post['full_sum'] = sum
+    data_post['comment'] = comment
+    # data_post['contact'] = web_app_message.contact
+
+
 @dp.pre_checkout_query_handler(lambda q:True)
 async def checkout_process(pre_checkout_query: PreCheckoutQuery):
-    print(pre_checkout_query.id)
+    data_post['user_id'] = pre_checkout_query.from_user.id
+    data_post['name'] = pre_checkout_query.order_info.name
+    data_post['phone_number'] = pre_checkout_query.order_info.phone_number
+    data_post['email'] = pre_checkout_query.order_info.email
+    data_post['address'] = pre_checkout_query.order_info.shipping_address.to_python()
+    print(data_post)
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
 
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: Message):
     await bot.send_message(message.chat.id, "Платіж успішний!")
 
+def post_succes_data():
+    pass
