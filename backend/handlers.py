6d6480885e8eab786f1bcaf201933b6360ec98bd
kopachmaksym <mk410404@gmail.com>
2023-04-29 21:11:40 +0300

    Final version. Added LiqPay payment

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,6 +1,6 @@
 from buy_process import dp, bot
 from config import PAYMENT_TOKEN
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, Invoice
 from aiogram.types.message import ContentType
 import re
 from keyboads import keyboard
@@ -18,7 +18,6 @@ async def buy_process(web_app_message):
     data = web_app_message.web_app_data.data.split('#')
     data = [i.split(':') for i in data]
     data.pop(0)
-    # print(data)
 
     for item in data:
         if item[1] == '0':
@@ -29,22 +28,22 @@ async def buy_process(web_app_message):
                            title='Покупка мікрочіпів',
                            description=f'Коментар: {comment}',
                            provider_token=PAYMENT_TOKEN,
-
-                           currency='uah',
+                           currency='UAH',
                            need_email=True,
                            need_phone_number=True,
-                           need_shipping_address= True,
+                           need_shipping_address=True,
                            prices=PRICE,
-                           # start_parameter='',
+                           start_parameter='example',
                            photo_url='https://encrypted-tbn0.gstatic.com/'
                                      'images?q=tbn:ANd9GcSxeOQ6x3CbY3R1lhNWI_s6dmi1Qcg7zocY_g&usqp=CAU',
                            payload='some_invoice')
 
 @dp.pre_checkout_query_handler(lambda q:True)
 async def checkout_process(pre_checkout_query: PreCheckoutQuery):
-    # print(pre_checkout_query.id)
+    print(pre_checkout_query.id)
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: Message):
     await bot.send_message(message.chat.id, "Платіж успішний!")
+
