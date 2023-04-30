9e7bf2d5145249d4d82858a4a1233662ac2098c0
kopachmaksym <mk410404@gmail.com>
2023-04-30 14:58:15 +0300

    Addet a way to save contacts to group channel (for admin)

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,11 +1,9 @@
 from buy_process import dp, bot
-from config import PAYMENT_TOKEN
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, Invoice
+from config import PAYMENT_TOKEN, CHAT_GROUP
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
 from aiogram.types.message import ContentType
 import re
 from keyboads import keyboard
-
-data_post = {}
 
 @dp.message_handler(commands=['start'])
 async def start(message: Message):
@@ -13,20 +11,33 @@ async def start(message: Message):
 
 @dp.message_handler(content_types='web_app_data')
 async def buy_process(web_app_message):
-    # print(web_app_message.web_app_data.data)
+    pr = {}
+    PRICE = []
+    global data_post
+    data_post = {}
+
     m = re.search(r' <comment>.*<comment>', web_app_message.web_app_data.data)
     comment = m.group(0).replace('<comment>', '').strip()
-    PRICE = []
     data = web_app_message.web_app_data.data.replace(m.group(0), '')
     data = data.split('#')
     data = [i.split(':') for i in data]
     data.pop(0)
+
     sum = 0
     for item in data:
         if item[1] == '0':
             continue
         PRICE.append(LabeledPrice(label=f'{item[0]} x{item[1]}', amount=int(item[2])*int(item[1])*100))
+        pr[f'{item[0]} x{item[1]}'] = str(int(item[2])*int(item[1])*100) + ' UAH'
         sum += int(item[2])
+
+    full_name, link = await get_name(web_app_message.from_user.id, web_app_message.chat.id)
+
+    data_post['Повне імя'] = full_name
+    data_post['Товар'] = pr
+    data_post['Ціна'] = str(sum) + " UAH"
+    data_post['Коментарій'] = comment
+    data_post['Контакт'] = link
 
     await bot.send_invoice(web_app_message.chat.id,
                            title='Покупка мікрочіпів',
@@ -42,27 +53,38 @@ async def buy_process(web_app_message):
                                      'images?q=tbn:ANd9GcSxeOQ6x3CbY3R1lhNWI_s6dmi1Qcg7zocY_g&usqp=CAU',
                            payload='some_invoice')
 
-
-    data_post['prices'] = PRICE
-    data_post['full_sum'] = sum
-    data_post['comment'] = comment
-    data_post['contact'] = web_app_message.contact
-
-
 @dp.pre_checkout_query_handler(lambda q:True)
 async def checkout_process(pre_checkout_query: PreCheckoutQuery):
-    data_post['user_id'] = pre_checkout_query.from_user.id
-    data_post['name'] = pre_checkout_query.order_info.name
-    data_post['phone_number'] = pre_checkout_query.order_info.phone_number
-    data_post['email'] = pre_checkout_query.order_info.email
-    data_post['address'] = pre_checkout_query.order_info.shipping_address.to_python()
-    await bot.send_message('-1001914657979', data_post)
+    data_post['User ID'] = pre_checkout_query.from_user.id
+    data_post['Телефон'] = pre_checkout_query.order_info.phone_number
+    data_post['Пошта'] = pre_checkout_query.order_info.email
+    data_post['Адресс'] = pre_checkout_query.order_info.shipping_address.to_python()
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
-
 
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: Message):
     await bot.send_message(message.chat.id, "Платіж успішний!")
+    await message_print()
 
-def post_succes_data():
-    pass
+async def message_print():
+    ptext = ''
+
+    for key, value in data_post.items():
+        if type(value) == dict:
+            ptext += f'{key}: \n'
+            for key2, value2 in value.items():
+                ptext += f'\t\t\t-{key2.replace("_", " ").capitalize()}: {value2}\n'
+        else:
+            ptext += f'{key}: {value}\n'
+
+    await bot.send_message(CHAT_GROUP, ptext)
+
+async def get_name(user_id: int, chat_id: int):
+    link = None
+    chat_member = await bot.get_chat_member(chat_id, user_id)
+    user = await bot.get_chat(user_id)
+    full_name =chat_member.user.full_name
+    if user.username:
+        link = f'https://t.me/{user.username}'
+
+    return full_name, link
