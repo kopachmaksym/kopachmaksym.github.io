5cabbc8f0b0d636a15ccd20c3f09053a44ee9cbe
kopachmaksym <mk410404@gmail.com>
2023-05-30 15:01:28 +0300

    Returned value to default

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ prices['Datamars'] = 182
 prices['animal-id min'] = 110
 prices['a-chip PREMIUM cannula'] = 134
 prices['a-chip PREMIUM mini'] = 182
-prices['animal-id pro 2,12x12mm'] = 1
+prices['animal-id pro 2,12x12mm'] = 102
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
